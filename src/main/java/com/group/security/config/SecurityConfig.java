67efa3dbbc531a4908178a3f4c8027ab84f05fa3
yuda3 <yuda3@hotmail.com>
2024-06-22 17:16:16 +0900

    Bean Setting

--- src/main/java/com/group/security/config/SecurityConfig.java
+++ src/main/java/com/group/security/config/SecurityConfig.java
@@ -1,9 +1,24 @@
 package com.group.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(authz -> authz
+//                .requestMatchers("/ui/**").permitAll()
+//                .requestMatchers("/admin/**").hasRole("ADMIN")
+//                .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+//                .anyRequest().authenticated()
+                .requestMatchers("/api/**").authenticated()
+                .requestMatchers("/book/**").authenticated()
+                .anyRequest().permitAll()
+        ).build();
+    }
 
 }
